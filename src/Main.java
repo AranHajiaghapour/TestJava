2d9e489c802d535b118a672be262b41fae20552e
AranHajiaghapour <133792665+AranHajiaghapour@users.noreply.github.com>
2023-09-18 19:48:50 +0200

    Testing Different Methods

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+
+import java.awt.*;
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+
+//        Date time = new Date();
+//        System.out.println(time);
+
+
+
+//                          This is a example of refrence types
+//        Point point1 = new Point(1,2);
+//        Point point2=point1 ;
+//        System.out.println(point1);
+
+
+
+
+//                          Example of True False cheking of the String
+//    String Text ="Hello Arad";
+//    boolean checking= Text.endsWith("d");
+//    System.out.println(checking);
+
+
+
+//
+        String Test = new String("sham bia pish man");
+
+        System.out.println(Test.replace("s","z") );
+
+
+
+    }
+}
